c94b1cf2f9a8bda8df68ed88a7e46dc7be828e0f
Max Merkuryev <maxmerkury13@gmail.com>
2022-10-11 22:23:27 +0700

    add moon

---
+++ Assets/Scripts/Interactable/testinteractable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Interactable {
+	public class testinteractable : MonoBehaviour, IInteractable {
+		public bool Enabled => true;
+		public string ActionName => "pick up";
+		public ActionType ActionType => ActionType.Hold;
+		
+		public void Interact() {
+			Debug.Log("INTERACTED AAAAAAAA");
+		}
+	}
+}

571e20eba311ffff53e09817a825738e2ac73fa7
Max Merkuryev <maxmerkury13@gmail.com>
2022-10-16 09:08:21 +0700

    add spookify

---
+++ Assets/Scripts/SpookifySystem/Spookify.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace SpookifySystem {
+	public class Spookify : MonoBehaviour {
+		[SerializeField] private AudioSource _ambienceSource; 
+		[SerializeField] private AudioSource _lofiSource;
+
+		[SerializeField] private AudioClip _ambience;
+		[SerializeField] private AudioClip[] _lofi;
+
+		private void Awake() {
+			_ambienceSource.clip = _ambience;
+			_ambienceSource.loop = true;
+			_ambienceSource.Play();
+			
+			PlayLofi();
+		}
+
+		private void PlayLofi() {
+			_lofiSource.clip = _lofi[Random.Range(0, _lofi.Length)];
+			_lofiSource.Play();
+			Invoke(nameof(PlayLofi), _lofiSource.clip.length + Random.Range(1f, 5f));
+		}
+	}
+}

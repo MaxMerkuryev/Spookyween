3c8a77cd6dde23b03a10633346c5ba406d72899b
Max Merkuryev <maxmerkury13@gmail.com>
2022-10-18 14:06:35 +0700

    wisp puzzle

---
+++ Assets/Scripts/Wispers/Wisp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Player;
+using UnityEngine;
+
+namespace Wispers {
+	public class Wisp : MonoBehaviour {
+		[SerializeField] private ParticleSystem _particles;
+		[SerializeField] private SphereCollider _collider;
+		[SerializeField] private Light _light;
+		
+		private int _index;
+		private Action<int> _onTrigger;
+
+		public void Init(int index, Action<int> action) {
+			_index = index;
+			_onTrigger = action;
+			Disable();
+		}
+		
+		private void OnTriggerEnter(Collider other) {
+			if (other.TryGetComponent(out PlayerController p)) {
+				_onTrigger?.Invoke(_index);
+			}
+		}
+
+		public void Enable() {
+			_light.enabled = true;
+			_collider.enabled = true;
+			_particles.Play();
+		}
+
+		public void Disable() {
+			_light.enabled = false;
+			_collider.enabled = false;
+			_particles.Stop();
+		}
+	}
+}
---
+++ Assets/Scripts/Wispers/WispPuzzle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Alchemy;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Wispers {
+	public class WispPuzzle : PotionEffectReceiver {
+		[SerializeField] private Wisp[] _wispers;
+		[SerializeField] private ParticleSystem _targetParticles;
+		[SerializeField] private GameObject _target;
+
+		private Wisp _currentWisp;
+		private bool _passed;
+		
+		private void Awake() {
+			for (int i = 0; i < _wispers.Length; i++) {
+				_wispers[i].Init(i, OnTrigger);
+			}
+		}
+
+		private void OnTrigger(int index) {
+			_currentWisp?.Disable();
+
+			if (index + 1 >= _wispers.Length) {
+				_passed = true;
+				_targetParticles.Play();
+				DOTween.Sequence().InsertCallback(0.1f, ()=> _target.SetActive(true));
+				return;
+			}
+
+			_currentWisp = _wispers[index + 1];
+			_currentWisp.Enable();
+		}
+
+		protected override PotionType _potionType => PotionType.GhostBreath;
+		
+		protected override void OnPotionDrinkAction() {
+			if(_passed)	return;
+			OnTrigger(-1);
+		}
+
+		protected override void OnPotionEndAction() {
+			if (_passed) return;
+			_currentWisp?.Disable();
+		}
+	}
+}

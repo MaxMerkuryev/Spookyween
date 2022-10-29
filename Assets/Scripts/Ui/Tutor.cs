44764d363aac68a781398115941b94a004f29406
Max Merkuryev <maxmerkury13@gmail.com>
2022-10-30 01:42:34 +0700

    add tutor

---
+++ Assets/Scripts/Ui/Tutor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DG.Tweening;
+using SfxSystem;
+using UnityEngine;
+
+namespace Ui {
+	public class Tutor : MonoBehaviour {
+		[SerializeField] private RectTransform _wasd;
+		[SerializeField] private RectTransform _space;
+		
+		private void Awake() {
+			_wasd.localScale = _space.localScale = Vector3.zero;
+			DOTween.Sequence()
+				.InsertCallback(1f, () => ShowTutor(_wasd))
+				.InsertCallback(2f, () => ShowTutor(_space))
+				.InsertCallback(5f, () => HideTutor(_wasd))
+				.InsertCallback(6f, () => HideTutor(_space))
+				.InsertCallback(10f, () => gameObject.SetActive(false));
+		}
+
+		private void ShowTutor(RectTransform tutor) {
+			tutor.DOScale(1f, 0.5f).SetEase(Ease.OutBack);
+			SfxPlayer.Play(SfxType.Pickup);
+		}
+
+		private void HideTutor(RectTransform tutor) {
+			tutor.DOScale(0f, 0.5f).SetEase(Ease.InBack);
+			SfxPlayer.Play(SfxType.Pickup);
+		}
+	}
+}
